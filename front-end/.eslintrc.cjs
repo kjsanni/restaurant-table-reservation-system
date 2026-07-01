d050d4b63bdc11da863301674d8be390e7913b08
Kobina John Sanni <kjsanni@vibespotgh.com>
2026-07-01 15:44:31 +0000

    chore: add TypeScript ESLint parser for Vue components
    - Install typescript and @typescript-eslint/parser
    - Extend eslint with @vue/eslint-config-typescript
    - Add defineOptions global for App.vue

--- front-end/.eslintrc.cjs
+++ front-end/.eslintrc.cjs
@@ -6,10 +6,12 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-essential",
+    "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
   ],
   parserOptions: {
     ecmaVersion: 13,
+    parser: "@typescript-eslint/parser",
   },
   rules: {
     "vue/no-unused-vars": "error",
