210bc7123bb57725507c1cd5f0b3933fc2eb9260
Kobina John Sanni <kjsanni@vibespotgh.com>
2026-07-02 18:03:04 +0000

    fix: change customer phone column length to STRING(15)

---
+++ back-end/src/db/migrations/20260702000002-change-customer-phone-length.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn("Customers", "phone", {
+      type: Sequelize.STRING(15),
+      allowNull: false,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.changeColumn("Customers", "phone", {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    });
+  },
+};
