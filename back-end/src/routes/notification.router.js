75ebfe63ada6af187e83df24d4100a0581d96875
Kobina John Sanni <kjsanni@vibespotgh.com>
2026-07-17 10:23:46 +0000

    chore: remove remaining unused imports (fs, tryCatchHandler) on main

--- back-end/src/routes/notification.router.js
+++ back-end/src/routes/notification.router.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const tryCatchHandler = require("../middleware/tryCatch");
 const httpMethodError = require("../middleware/httpMethodError");
 const notificationController = require("../controllers/notification.controller");
 const { writeRoute } = require("../utils/routeHelpers");
